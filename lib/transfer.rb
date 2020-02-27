d89c43f59fdecb291aee55263e8adc439e045ccf
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-27 22:43:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
  end
  
  def execute_transaction 
-   if self.valid?
+   if @status != 'complete' && @status != 'rejected'
      if @sender.balance > @amount
        @sender_safety = @sender
        @receiver_safety = @receiver 
