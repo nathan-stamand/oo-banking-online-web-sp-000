6ba9743644d4d45e12d2dc27155116aada661c4e
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-27 21:48:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
-  # your code here
+ 
+ 
+ def initialize(sender, receiver, amount)
+   @sender = sender 
+   @receiver = receiver
+   @amount = amount
+   @status = "pending"
+ end 
+ 
+ 
+ 
 end
